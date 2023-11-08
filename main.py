d04be5ce9926db943699073506c6b8ebfb5daf4a
gustavo8555 <gustavo8555@hotmail.com>
2023-11-08 03:02:10 +0000

    FEAT: adds broker communication

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import serial
+from mqtt_publisher import MQTT
 
 ser = serial.Serial('/dev/ttyUSB0', 9600) # Lendo os dados da porta serial
+comunicacao = MQTT("mqtt-dashboard.com")
 
 def enviar_mensagem(msg):
     print("Enviando NTU: ", msg)
-    pass
+    comunicacao.enviar_mensagem(msg)
 
 while True:
     data = ser.readline().decode('utf-8').strip()
--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,8 +1,16 @@
 import paho.mqtt.client as mqtt
 
-client = mqtt.Client(client_id="", protocol=mqtt.MQTTv311)
-conection = client.connect(host="mqtt-dashboard.com")
+class MQTT:
+    # host
+    def conectar_client(self, host):
+        client = mqtt.Client(client_id="", protocol=mqtt.MQTTv311)
+        connection = client.connect(host=host)
+        return (client, connection)
+    
+    def enviar_mensagem(self, msg):
+        self.client.publish(topic="casa/sensor_turbidez", payload=msg)
+    
+    def __init__(self, host):
+        print("Criando conexão")
+        self.client, self.connection = self.conectar_client(host)
 
-client.publish(topic="casa/sensor_turbidez", payload="ligado")
-
-print("Mensagem enviada")
