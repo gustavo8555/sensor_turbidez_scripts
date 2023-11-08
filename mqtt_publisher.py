0f360ee96d022c069f08c88e48494ebf8a22ba47
gustavo8555 <gustavo8555@hotmail.com>
2023-11-08 20:16:52 -0300

    REVERT: clean old test file

--- mqtt_publisher.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client(client_id="", protocol=mqtt.MQTTv311)
-conection = client.connect(host="mqtt-dashboard.com")
-
-client.publish(topic="casa/sensor_turbidez", payload="ligado")
-
-print("Mensagem enviada")
